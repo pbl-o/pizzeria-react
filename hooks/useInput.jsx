5834305924e98508d65c2aa0d83f37cceff9c588
pblo <pdiazarenas541@gmail.com>
2025-10-31 13:07:15 +0000

    adding custom hooks

---
+++ hooks/useInput.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+
+const useInput = (initialValue) => {
+const [value, setValue] = useState(initialValue)
+
+const handleChange = ({target}) => {
+    setValue(target.value)
+}
+
+return{
+    value,
+    onChange : handleChange, 
+}
+}
+
+export default useInput
